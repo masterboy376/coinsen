0bc317b6d9b74e46f74f5b0d605b3829c16b5b75
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-09-07 12:54:23 +0000

    app update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Proposal from "./pages/Proposal";
-import moralisLogo from "./images/Moralis.png";
 import { ConnectButton } from "web3uikit";
 
 const App = () => {
   return (
     <>
       <div className="header">
-        {/* <img width="160px" src={moralisLogo} alt="logo" /> */}
         <span style={{fontSize: "25px", fontWeight: "bold" , color:"white"}}>DAO</span>
         <ConnectButton />
       </div>
